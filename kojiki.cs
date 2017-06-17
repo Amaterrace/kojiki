28f35dae7cb4d14b9faf60e2833b9a7f34c3f3e4
thinceller <kohei.kyoto.0122@gmail.com>
2017-06-17 23:26:53 +0900

    first remote commit

--- kojiki.cs
+++ kojiki.cs
@@ -3,10 +3,11 @@ using System.Windows.Forms;
 using System.Drawing;
 using NAudio.Wave;
 
-
 class Sample : Form
 {
-    private System.Media.SoundPlayer player = null;
+    AudioFileReader reader = new AudioFileReader("title_kari.wav");
+    WaveOut waveOut = new WaveOut();
+    private TrackBar tb;
     private Button startBt;
     private Button exitBt;
     private Button confBt;
@@ -20,27 +21,32 @@ class Sample : Form
     // コンストラクタ
     public Sample()
     {
-        const int w=800, h=600;
+        const int w = 800, h = 600;
         this.Text = "古事記";
         // サイズ固定（最大化とかはできる）
         this.FormBorderStyle = FormBorderStyle.FixedSingle;
-        this.Width = w;  this.Height = h;
+        this.Width = w; this.Height = h;
 
         // ボタン作成
         startBt = new Button();
         exitBt = new Button();
         confBt = new Button();
+        tb = new TrackBar();
         startBt.Text = "START";
+        confBt.Text = "CONFIG";
         exitBt.Text = "EXIT";
 
         // ボタン表示
-        double x=0.5*w-0.5*startBt.Width, y=0.5*h-0.5*startBt.Height;
+        double x = 0.5 * w - 0.5 * startBt.Width, y = 0.5 * h - 0.5 * startBt.Height;
         startBt.Location = new Point((int)x, (int)y);
-        exitBt.Location = new Point((int)x, (int)(y+2*startBt.Height));
+        confBt.Location = new Point((int)x, (int)(y + 2 * startBt.Height));
+        exitBt.Location = new Point((int)x, (int)(y + 4 * startBt.Height));
 
         // ボタン動作
         startBt.Click += new EventHandler(startBt_Click);
+        confBt.Click += new EventHandler(confBt_Click);
         exitBt.Click += new EventHandler(exitBt_Click);
+
 
         // マウスClick動作
         this.MouseClick += new MouseEventHandler(mouseClick);
@@ -48,67 +54,70 @@ class Sample : Form
         TitleDraw();
     }
 
-    // タイトル表示
     public void TitleDraw()
     {
         // 背景
         this.BackgroundImage = Image.FromFile("kojiki_memu_back.bmp");
+        reader.Position = 0;
+        waveOut.Init(reader);
+        waveOut.Volume = 0.1f;
+        waveOut.Play();
 
-        // BGM再生
-        player = new System.Media.SoundPlayer("title_kari.wav");
-        player.PlayLooping();
+        this.Controls.Remove(tb);
 
-        // ボタン配置
         startBt.Parent = this;
+        confBt.Parent = this;
         exitBt.Parent = this;
 
         flag = false;
     }
 
-    // ゲーム画面表示
-    public void GameDraw()
+    //======================ボタン================================
+    public void startBt_Click(Object sender, EventArgs e)
     {
-        // 背景
         this.BackgroundImage = Image.FromFile("ana.bmp");
-
-        // BGM停止
-        player.Stop();
-        player.Dispose();
-        player = null;
 
         // ボタン消去
         this.Controls.Remove(startBt);
+        this.Controls.Remove(confBt);
         this.Controls.Remove(exitBt);
+
+        waveOut.Stop();
 
         flag = true;
     }
 
-//======================ボタン================================
-    public void startBt_Click(Object sender, EventArgs e)
+    public void confBt_Click(Object sender, EventArgs e)
     {
-        GameDraw();
+        this.BackgroundImage = Image.FromFile("conf.bmp");
+        // ボタン消去
+        this.Controls.Remove(startBt);
+        this.Controls.Remove(confBt);
+        this.Controls.Remove(exitBt);
+
+        tb.Parent = this;
+
+        flag = true;
+
     }
 
     public void exitBt_Click(Object sender, EventArgs e)
     {
-        string msg = "ゲームを終了しますか？";
-        DialogResult result = MessageBox.Show(msg, "ゲーム終了", MessageBoxButtons.YesNo);
-        if(result == DialogResult.Yes)
-        {
-            Application.Exit();
-        }
+        Application.Exit();
     }
 
-//=======================マウス================================
-    public void mouseClick(object sender, MouseEventArgs e) {
-        string msg = "タイトルへ戻りますか？";
-        if(e.Button == MouseButtons.Right)
+    //=======================マウス================================
+    public void mouseClick(object sender, MouseEventArgs e)
+    {
+        string msg = "タイトルへ戻りますか？";	// クライアント座標
+        if (e.Button == MouseButtons.Right)
         {
-            if(flag)
+            if (flag)
             {
                 DialogResult result = MessageBox.Show(msg, "メニュー", MessageBoxButtons.YesNo);
-                if(result == DialogResult.Yes)
+                if (result == DialogResult.Yes)
                 {
+                    waveOut.Stop();
                     TitleDraw();
                 }
             }
